aec20e43687ec0ec3eaf5cedd75a2c6f0e7e4d6c
Malachi9311 <ntuthuko93dlamini@gmail.com>
2023-08-03 10:06:29 +0200

    task08

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-'''Task 8's module.
-'''
+"""
+Module for task 08
+"""
 from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    '''Creates a multiplier function.
-    '''
+    """
+    Creates a multiplier function
+    """
     return lambda x: x * multiplier
